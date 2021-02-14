9daccff970755c1cee6bc471b74d8bf1042c3eb5
stixguy <44690263+stixguy@users.noreply.github.com>
2021-02-14 09:42:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="typewriter.js"></script>
+
+<link rel="stylesheet" type="text/css" href="typewriter.css">
+</head>
+<style>
+
+</style>
+
+<body>
+<hr class="hr">
+
+<div class="row">
+  <div class="column1" >
+    <p class="p4">The project “Encased: A Three-Dimensional Point and Click Crime Solving Puzzle Game”
+ is developed by combining the concept of three-dimensional graphics and investigative 
+ and forensic capabilities, to help people enhance their logical, improvisation skills and 
+ critical thinking</p>
+  </div>
+  
+  <div class="column2" >
+  <h1>ENCASED</h1>
+    <h2>Point and Click Crime Solving Puzzle Game</h2>
+   
+  </div>
+</div>
+
+<pre class="p3">Marshmallow                 1GB RAM <button class="button button3">Download</button></pre>
+<p class="p2">Copyright © 2019</p>
+
+</div>
+<hr class="hr">
+</body>
+</html>
